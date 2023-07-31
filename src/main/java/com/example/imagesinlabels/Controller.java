d53c2838f7dbdd847fe4ab50befef60d1b2d9b92
DublinJim <59180380+DublinJim@users.noreply.github.com>
2023-07-31 14:20:12 -0400

    arraylist of images created

--- src/main/java/com/example/imagesinlabels/Controller.java
+++ src/main/java/com/example/imagesinlabels/Controller.java
@@ -2,10 +2,12 @@ package com.example.imagesinlabels;
 
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
@@ -18,9 +20,22 @@ public class Controller implements Initializable {
     public Button btnHold3;
     public Button btnBet;
     public Button btnSpin;
+    private ArrayList<Image> imageList = new ArrayList<>();
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+        imageList.add(new Image("bell.png"));
+        imageList.add(new Image("cherry.png"));
+        imageList.add(new Image("grapes.png"));
+        imageList.add(new Image("lemon.png"));
+        imageList.add(new Image("melon.png"));
+        imageList.add(new Image("orange.png"));
+        imageList.add(new Image("seven.png"));
+        imageList.add(new Image("strawberry.png"));
+
+        drumImg1.setImage(imageList.get(0));
+        drumImg2.setImage(imageList.get(0));
+        drumImg3.setImage(imageList.get(0));
     }
 }
